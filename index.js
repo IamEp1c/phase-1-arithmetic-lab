a9ca3289da8b284f9f11b475fd84686774676b71
Waqas Anjum <Qanjum96@gmail.com>
2022-04-04 17:34:24 -0400

    commit tree b3b00c74796bff1348e6214e1684b9dd3b71c6d2

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 16
+let num4 = 4
+
+let mod = num3 / num4
+
+let max = Math.max(1, 5, 7, 10, 20)
